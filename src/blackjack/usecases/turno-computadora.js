38f27088e6ee047a8094358acfcada2871b6989d
Gonzalo_01 <gonzalo.millanao@gmail.com>
2024-05-04 02:16:43 -0400

    first commit

---
+++ src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,36 @@
+import { pedirCarta, valorCarta, crearCartaHTML } from './index';
+
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = [] ) => {
+
+    if ( !puntosMinimos ) throw new Error('Puntos mínimos son necesarios');
+    if ( !puntosHTML ) throw new Error('Argumento puntosHTML es necesario');
+
+    let puntosComputadora = 0;
+ 
+    do {
+        const carta = pedirCarta( deck );
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+        
+        const imgCarta = crearCartaHTML( carta );
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+}
